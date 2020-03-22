0460cef11b3feffac471e7cb3cc0c4ea339c1385
Usiein <usein.osmanov.ct@gmail.com>
2020-03-23 00:06:33 +0200

    Initial commit on Flask-blog repo

---
+++ app/models.py
@@ -0,0 +1,35 @@
+from werkzeug.security import generate_password_hash as gph, check_password_hash as cph
+from datetime import datetime
+from flask_login import UserMixin
+from app import db, login
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = gph(password)
+
+    def check_password(self, password):
+        return cph(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
