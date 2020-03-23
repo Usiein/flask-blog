ed553f05be369373edc4d66ee891dd77136a27bc
Usiein <usein.osmanov.ct@gmail.com>
2020-03-24 01:13:49 +0200

    User profile editor added

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash as gph, check_password_hash as cph
 from datetime import datetime
 from flask_login import UserMixin
+from hashlib import md5
 from app import db, login
 
 
@@ -10,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -23,6 +26,10 @@ class User(UserMixin, db.Model):
     @login.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
