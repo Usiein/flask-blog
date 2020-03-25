173e872b491a351490f5c63e567f3be04e438337
Usiein <usein.osmanov.ct@gmail.com>
2020-03-25 22:11:21 +0200

    Validation error fixed as long with minor UI bug

--- app/forms.py
+++ app/forms.py
@@ -35,3 +35,8 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username unavailable, please try different')
