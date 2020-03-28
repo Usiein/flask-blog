bf6e598cefd0dd32e11a96a3e128ddfad8d57da2
Usiein <usein.osmanov.ct@gmail.com>
2020-03-29 00:44:29 +0200

    Pagination added

--- app/forms.py
+++ app/forms.py
@@ -47,3 +47,9 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Username unavailable, please try different.')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
